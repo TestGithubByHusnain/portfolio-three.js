0ef4e0fc2f558643aef9b753733e743801277cab
Husnain <husnainali41940@gmail.com>
2025-01-09 01:51:25 -0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,276 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'About',
+      href: '#about',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      id: 1,
+      name: 'Developers Hub',
+      position: 'Front-End Developer (Intern)',
+      img: 'assets/developershub.png',
+      review:
+        'We appreciate Husnain for his outstanding performmance, skills, and significant contributions demonstrated throughout the internship period. ',
+    },
+    {
+      id: 2,
+      name: 'Code Alpha',
+      position: 'Front-End Developer (Intern)',
+      img: 'assets/codealpha.png',
+      review:
+        'Husnain exhibited performmance in this role and made a valuable contribution to our organization. during the internship period. He has excellent analytical skills and he quickly accquired new skills and adeptly adapted to emerging technologies, demonstrating a high level of productivity.',
+    },
+    {
+      id: 3,
+      name: 'CodexCue',
+      position: 'React Developer (Intern)',
+      img: 'assets/codexcue.png',
+      review:
+        'Husnain has shown outstanding dedication, professioalism, and a strong ability to collaborate effectively with our team. The innovation approach and diligent work ethic have made a significant impact on our projects. We are confident that Husnain Ali will excel in any future role they undertake.',
+    },
+ 
+  ];
+  
+  export const myProjects = [
+    {
+      title: 'Apple-Iphone 15 Pro',
+      desc: 'Recreating the Applr iphone 15 pro website, combining GSAP animations and three.js 3D effects. From custom animations to 3D models.',
+      subdesc:
+  'Built this immersive web by using react.js, gsap for animations, react-three-fibre, three.js.',
+      // href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+      texture: '/textures/project/apple.mp4',
+      // logo: '/assets/project-logo1.png',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'Three.js',
+          path: '/assets/threejs.webp',
+        },
+        {
+          id: 4,
+          name: 'gsap',
+          path: '/assets/gsap.webp',
+        },
+      ],
+    },
+    {
+      title: 'Redefine-Awards',
+      desc: 'A visually captivating website featuring scroll-triggered animations, geometric transitions and engagaing video storytelling.',
+      subdesc:'Built by using react.js, gsap, tailwindcss',
+      // href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+      texture: '/textures/project/awards.mp4',
+      // logo: '/assets/project-logo2.png',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'gsap',
+          path: '/assets/gsap.webp',
+        },
+        {
+          id: 4,
+          name: 'threejs',
+          path: '/assets/threejs.webp',
+        },
+      ],
+    },
+    {
+      title: 'BlockForge',
+      desc: 'BlockForge the future of blockchain.',
+      subdesc:
+        'Built by using React.js, Astro.js, framermotion, tailwindcss.',
+      // href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+      texture: '/textures/project/blockforge.mp4',
+      // logo: '/assets/project-logo3.png',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/astrojs.webp',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Xora SaaS-landing-page',
+      desc:'Xora is a Modern ui/ux developed using react.js and tailwindcss that exemplifies modern ui/ux principles.Its sleek design, seamless animations, and overall user experience set a high standard, serving as a refrence or inspiration for future modern applicationsbor websites in general.',
+      subdesc:
+        'Built by using vite, react.js , tailwindcss',
+      // href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+      texture: '/textures/project/xora.mp4',
+      // logo: '/assets/project-logo4.png',
+      logoStyle: {
+        backgroundColor: '#0E1F38',
+        border: '0.2px solid #0E2D58',
+        boxShadow: '0px 0px 60px 0px #2F67B64D',
+      },
+      spotlight: '/assets/spotlight4.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'vite',
+          path: 'assets/vite.webp',
+        },
+        // {
+        //   id: 3,
+        //   name: 'vite',
+        //   path: '/assets/typescript.png',
+        // },
+        {
+          id: 4,
+          name: 'tailwind',
+          path: '/assets/tailwindcss.png',
+        },
+      ],
+    },
+    {
+      title: 'Light SaaS Landing page',
+      desc: 'A digital masterpiece with stunning animations, and responsive design using cutting edeg tools like React.js, Framermotion, tailwindcss.',
+      subdesc:
+        'Built by using react.js, framermotion, tailwindcss.',
+      // href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+      texture: '/textures/project/saas.mp4',
+      // logo: '/assets/project-logo5.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight5.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+  ];
+  
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+  };
+  
+  export const workExperiences = [
+    {
+      id: 1,
+      name: 'Codexcue Software Solutions',
+      pos: 'React Developer (Intern)',
+      duration: '1st November 2024 to 30th November 2024',
+      title: "I worked as a React developer at CodexCue, specializing in building efficient, scalable applications, implementing advanced React features, optimizing performance, and ensuring seamless user experiences through responsive and interactive design.",
+      icon: '/assets/codexcue.png',
+      animation: 'victory',
+    },
+    {
+      id: 2,
+      name: 'CodeAlpha',
+      pos: 'Front-End Web Developer (Intern)',
+      duration: '1st October 2024 to 30th October 2024',
+      title: "I interned at CodeAlpha as a front-end web developer, gaining experience in creating responsive interfaces, improving user experiences, and collaborating on dynamic projects using modern web development tools and practices.",
+      icon: '/assets/codealpha.png',
+      animation: 'clapping',
+    },
+    {
+      id: 3,
+      name: 'DevelopersHub Corporation',
+      pos: 'Front-End Web Developer (Intern)',
+      duration: '1st September 2024 to 15th October 2024',
+      title: "DevelopersHub helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+      icon: '/assets/developershub.png',
+      animation: 'salute',
+    },
+  ];
